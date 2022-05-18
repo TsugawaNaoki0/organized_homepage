2c9f010cdc3391d451d8d758caf1df12afa77b42
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-05-18 15:57:57 +0900

    Add files via upload

---
+++ data/camera.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" href="./../../home.css">
+   <title>HallowinGhost</title>
+    <link rel="icon" href="ghost.png"><!-- タイトルにアイコンを設定 -->
+    <link rel="apple-touch-icon" href="icon.png"><!-- iphone のアイコンを設定 -->
+ </head>
+ <body>
+    <div class="main">
+            <br>
+            <h1>カメラ</h1>
+            <br>
+            <br>
+            <form action="./code/cam_transfer.php" method="post">
+              <!-- <input type="textarea" name="confirm" value=""> -->
+              <label for="confirm0"><h3>カメラ</h3></label><br>
+              <select name="confirm0" class="form">
+                  <!-- <option value="blue">青</option> -->
+                  <option value="kanagawa">神奈川県</option>
+              </select>
+
+
+              <input type="submit" value="-----" class="form">
+            </form>
+
+            <br>
+            <br>
+
+
+
+
+    </div>
+
+ </body>
+</html>
