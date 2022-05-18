973fa7f612626e84cb32d9d5f2532f8beea0a30f
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-05-18 15:56:25 +0900

    Add files via upload

---
+++ data/code/cam_transfer.php
@@ -0,0 +1,82 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+ <head>
+   <meta charset="utf-8">
+   <link rel="stylesheet" href="./../../../home.css">
+   <title>HallowinGhost</title>
+    <link rel="icon" href="ghost.png"><!-- タイトルにアイコンを設定 -->
+    <link rel="apple-touch-icon" href="icon.png"><!-- iphone のアイコンを設定 -->
+ </head>
+ <body>
+    <div class="main">
+            <br>
+            <h1>カメラ</h1>
+            <br>
+            <br>
+            <?php
+              if ($_POST["confirm0"] == "kanagawa");
+              echo "<a href='https://www.youtube.com/watch?v=xdt2N3o9B5Y' target='_blank'>久里浜遠望カメラ[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=KONP4VrgOsY' target='_blank'>湘南ライブカメラ[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=9ZYq4LYIhHI' target='_blank'>二子玉川カメラ[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=ycOMCBi6xws' target='_blank'>小田急線厚木駅付近カメラ[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=f8muETuC0kM' target='_blank'>三浦海岸の様子[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=K0vT2_0nYXU' target='_blank'>相模原市の相模湖[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=lKAKWcKyyjs' target='_blank'>三浦海岸(切替えあり)[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=u-MEeYrVOYw' target='_blank'>由比ヶ浜海岸[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.youtube.com/watch?v=lvlFx0PAJ-k' target='_blank'>相模川圏央道付近[Youtube]</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_11.html' target='_blank'>雨量水位情報(横浜)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_12.html' target='_blank'>雨量水位情報(川崎)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_13.html' target='_blank'>雨量水位情報(湘南)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_14.html' target='_blank'>雨量水位情報(三浦半島)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_15.html' target='_blank'>雨量水位情報(相模原)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_16.html' target='_blank'>雨量水位情報(県央)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_17.html' target='_blank'>雨量水位情報(足柄上)</a>";
+              echo "<br>";
+              echo "<br>";
+              echo "<a href='https://www.pref.kanagawa.jp/sys/suibou/web_general/suibou_joho/html/camera/list/p20101_18.html' target='_blank'>雨量水位情報(西湘)</a>";
+              echo "<br>";
+              echo "<br>";
+             ?>
+
+            <br>
+            <br>
+
+
+
+
+    </div>
+
+ </body>
+</html>
