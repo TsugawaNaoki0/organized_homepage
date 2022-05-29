afa97006afc3ea1177d7ca01e43c410f0b608bf3
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-05-29 22:23:35 +0900

    Add files via upload

---
+++ data/sample.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./../../home.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="main">
+      <br>
+      <br>
+      <h1>あいうえお</h1>
+      <figure>
+      <?php
+        echo "<a href='https://www.yahoo.co.jp/'><img src='./ichigo.jpeg' alt=''></a>";
+        echo "VS";
+        echo "<a href='https://qiita.com/carotene4035/items/e00076fe3990b9178cc0'><img src='./banana.jpg' alt=''></a>";
+       ?>
+      </figure>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+
+    </div>
+  </body>
+</html>
