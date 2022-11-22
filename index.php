df700e184a8a8993ca06918eeb0e5bb35c9e62f9
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-11-22 23:55:40 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./../home.css">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo shell_exec("export LANG=ja_JP.UTF-8; python3 ../log.py ".$_SERVER['PHP_SELF']);
+     ?>
+    <div class="main">
+      <br>
+      <br>
+      <h1>HOME</h1>
+      <h2><a href="./data/taihou.html">泰鵬大学(モデル：日大)</a></h2>
+      参考  https://generated.photos/
+      <br>
+      <br>
+      <h2><a href="./data/passwd_etc.html">ログインパスワード等</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/camera.php">カメラ</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/incident.html">事件</a></h2>
+      <br>
+      <br>
+      <h2><a href="http://35.77.185.198/"  target="_blank">掲示板</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/politics.html">政治</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/chousei.html">年末調整</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/religion.html">宗教</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/houritsu.html">法律用語</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/news_paper.html">新聞</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/payment.html">支払い</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/research.html">調べ学習</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/revolving.html">リボ払い計算式</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/zeikin.html">税金計算</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/map.html">地図</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/baseball.html">野球</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/soccer.html">J リーグ</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/human_resource.html">人びと</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/weather.html">天気</a></h2>
+      <br>
+      <br>
+      <h2><a href="./data/kabu_moving.html">株価の変動</a></h2>
+      <br>
+      <br>
+      <h2><a href='./data/fudousan.html'>不動産</a></h2>
+      <br>
+      <br>
+      <h2><a href='./data/museum.html'>美術館</a></h2>
+      <br>
+      <br>
+      <h2><a href='./data/toukei.html'>統計</a></h2>
+      <br>
+      <br>
+      <h2><a href='https://www.oshimaland.co.jp/' target='_blank'>大島てる</a></h2>
+      <br>
+      <br>
+      <br>
+      <br>
+
+    </div>
+  </body>
+</html>
