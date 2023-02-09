d677d34a44893930d2ca9badcd37486285bd25cd
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2023-02-09 19:45:39 +0900

    Add files via upload

---
+++ data/search.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./home.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="main">
+      <form class="" action="./search.php" method="post">
+        <div class="">
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <input type="text" name="text" value="" class="login">
+          <input type="submit" name="" value="検索" class="login">
+        </div>
+      </form>
+      <br>
+      <br>
+      ※　検索ワードは１つだけ
+      <br>
+      <br>
+      <br>
+      <?php
+        $sakana =  $_POST[text];
+        echo "検索ワード：「".$sakana."」";
+        ?>
+        <br>
+        <br>
+        <hr>
+        <br>
+        <?php
+        $momoniku = file(__DIR__ . '/links.txt');
+        $ebi = file(__DIR__ . '/urls.txt');
+
+        $yaki_doufu = count($momoniku, COUNT_RECURSIVE);
+        $momen_doufu = count($ebi, COUNT_RECURSIVE);
+
+        for ($i = 0; $i < $yaki_doufu; $i++){
+          if (strpos(substr($momoniku[$i], 8), $sakana) !== false){
+            $index_momoniku = substr($momoniku[$i], 0, 8);
+            for ($j = 0; $j < $momen_doufu; $j++){
+              // echo $index_momoniku;
+              if (strpos($ebi[$i], $index_momoniku) !== false){
+                // echo $index_momoniku;
+                echo "<a href=\"".substr($ebi[$i], 8)."\">".substr($momoniku[$i], 8)."</a>";
+                break;
+              }
+            }
+
+            ?>
+            <br>
+            <br>
+            <?php
+            }
+          }
+      ?>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+
+    </div>
+
+  </body>
+</html>
--- data/taihou.php
+++ data/taihou.php
@@ -5,6 +5,66 @@
     <title></title>
   </head>
   <body>
+    <form class="" action="./search.php" method="post">
+      <div class="">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <input type="text" name="text" value="" class="login">
+        <input type="submit" name="" value="検索" class="login">
+      </div>
+    </form>
+    <br>
+    <br>
+    ※　検索ワードは１つだけ
+    <br>
+    <br>
+    <br>
+    <?php
+      $sakana =  $_POST[text];
+      echo "検索ワード：「".$sakana."」";
+      ?>
+      <br>
+      <br>
+      <hr>
+      <br>
+      <?php
+      $momoniku = file(__DIR__ . '/links.txt');
+      $ebi = file(__DIR__ . '/urls.txt');
+
+      $yaki_doufu = count($momoniku, COUNT_RECURSIVE);
+      $momen_doufu = count($ebi, COUNT_RECURSIVE);
+
+      for ($i = 0; $i < $yaki_doufu; $i++){
+        if (strpos(substr($momoniku[$i], 8), $sakana) !== false){
+          $index_momoniku = substr($momoniku[$i], 0, 8);
+          for ($j = 0; $j < $momen_doufu; $j++){
+            // echo $index_momoniku;
+            if (strpos($ebi[$i], $index_momoniku) !== false){
+              // echo $index_momoniku;
+              echo "<a href=\"".substr($ebi[$i], 8)."\">".substr($momoniku[$i], 8)."</a>";
+              break;
+            }
+          }
+
+          ?>
+          <br>
+          <br>
+          <?php
+          }
+        }
+    ?>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
     <h1>泰鵬大学</h1>
     <hr>
     <hr>
