5f0d9bcdd611a55e8fcad2d93859a44162819274
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2023-02-18 22:07:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -37,6 +37,9 @@
       <h2><a href="../search/index.php">検索</a></h2>
       <br>
       <br>
+      <h2><a href="../zero_wiki/index.php">検索</a></h2>
+      <br>
+      <br>
       <h2><a href="./data/chousei.html">年末調整</a></h2>
       <br>
       <br>
